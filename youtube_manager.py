49f8f6eccbba857059329946d805e579d2649450
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-08-03 14:54:41 +0530

    implemented v1 of the learning python by project

--- youtube_manager.py
+++ youtube_manager.py
@@ -1,15 +1,77 @@
 def list_all_videos(videos):
-    pass
+    print("*" * 50)
+    for video in videos:
+        print(f"{video}\n")
+    print("*" * 50)
 
 def add_video(videos):
-    pass
-
+    current_number_videos = len(videos)
+    title = input("enter the title of the video: ")
+    duration = input("enter the duration of the video: ")
+    createdBy = input("enter the creator of the video: ")
+    
+    current_video = {
+        "id": current_number_videos + 1,
+        "title": title,
+        "duration": duration,
+        "createdBy": createdBy
+    }
+    
+    videos.append(current_video)
+    print("video is added successfully...")
+    
 def update_video(videos):
-    pass
+    if len(videos) <= 0:
+        print("no added videos yet...!")
+        return
 
+    while True:
+        video_id = int(input("enter the id of the video: "))
+        if video_id <= len(videos):
+            break
+        print(f"video with give id:{video_id} doesn't exists")
+    
+    print(f"Note: updates will be reflected in the corresponding old ones...")
+    new_title = input("enter a new title for the video (press enter, if you don't want): ")
+    new_duration = input("enter a new duration for the video (press enter, if you don't want): ")
+    new_createdBy = input("enter a new creator of the video (press enter, if you don't want): ")
+    
+    #[ {}, {}, {}, ... ]
+    if new_title.strip() != "":
+        print("title gets updated...!")
+        for video in videos:
+            if(video['id'] == video_id):
+                video['title'] = new_title
+                
+    if new_duration.strip() != "":
+        print("duration gets updated ...!")
+        for video in videos:
+            if(video['id'] == video_id):
+                video['duration'] = new_duration
+                
+    if new_createdBy.strip() != "":
+        print("createdBy gets updated...!")
+        for video in videos:
+            if(video['id'] == video_id):
+                video['createdBy'] = new_createdBy                
+    
 def delete_video(videos):
-    pass
-
+    if len(videos) <= 0:
+        print("no added videos yet...!")
+        return
+    
+    while True:
+        video_id = int(input("enter the id of the video: "))
+        if video_id <= len(videos):
+            break
+        print(f"video with give id:{video_id} doesn't exists")
+        
+    for video in videos:
+        if videos.index(video) == video_id:
+            videos.remove(video)
+            print(f"video with id:{video_id} and contents: {video} gets deleted...!")
+            break
+        
 def main():
     videos = []
     while True:
@@ -24,17 +86,17 @@ def main():
         
         match choice:
             case "1":
-                pass
+                list_all_videos(videos)
             case "2":
-                pass
+                add_video(videos)
             case "3":
-                pass
+                update_video(videos)
             case "4":
-                pass
+                delete_video(videos)
             case "5":
                 break
             case _: # default case:
-                print("Invalid option choosen, tyr again")
+                print("Invalid option choosen, try again")
 
 if __name__ == "__main__":
     main()
